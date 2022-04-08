1350d51477e330fce9bda92dbcb09545b7e7bc57
Ryan.K <ryankung@ieee.org>
2022-04-08 16:03:14 +0800

    basic test

---
+++ sim/tests/test_nat.rs
@@ -0,0 +1,33 @@
+use async_process::Command;
+use netsim_embed::Ipv4Range;
+use netsim_embed::Netsim;
+use netsim_embed::*;
+use netsim_embed_machine::Namespace;
+
+#[test]
+fn test_nat() {
+
+    run(async {
+        env_logger::init();
+        let mut netsim = Netsim::<String, String>::new();
+        let net1 = netsim.spawn_network(Ipv4Range::global());
+        let mut server = Command::new("ncat");
+        server.args(&["-l", "-4", "-p", "4242", "-c", "echo '<Hello World'"]);
+
+        let server = netsim.spawn_machine(server, None).await;
+        netsim.plug(server, net1, None).await;
+        let server_addr = netsim.machine(server).addr();
+        println!("Server Addr {}:4242", server_addr.to_string());
+
+        let _ns = Namespace::current().unwrap();
+        let ns_server = netsim.machine(server).namespace();
+        println!("{}", ns_server);
+        netsim.machine(server).namespace().enter().unwrap();
+
+        let mut cmd = Command::new("nc");
+        cmd.args(&[&*server_addr.to_string(), "4242"]);
+        let output = cmd.output().await.unwrap();
+        println!("response: {}", std::str::from_utf8(&output.stdout).unwrap());
+        //println!("error: {}", std::str::from_utf8(&output.stderr).unwrap());
+    });
+}
