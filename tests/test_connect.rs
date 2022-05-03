00c960c5dc6f9aca1c30517a1c1659aa8d9bfb1c
magine <magine233@gmail.com>
2022-05-03 14:15:27 +0800

    test connect by sdp

--- tests/test_connect.rs
+++ tests/test_connect.rs
@@ -9,9 +9,6 @@ pub mod test {
         let nat = create_nat()?;
         let node1 = create_node(&nat, Some("50000"))?;
         let node2 = create_node(&nat, Some("50000"))?;
-        println!("{:?}", nat);
-        println!("{:?}", node1);
-        println!("{:?}", node2);
 
         let ep1 = format!("http://127.0.0.1:{}", node1.pub_port.unwrap());
         let ep2 = format!("http://127.0.0.1:{}", node2.pub_port.unwrap());
@@ -35,6 +32,45 @@ pub mod test {
 
         let node2_peers = cli2.list_peers().await?.result;
         assert_eq!(node2_peers.len(), 1);
+        assert_eq!(node2_peers[0].address, node1.address()?);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_connect_peer_via_sdp_in_same_nat() -> Result<()> {
+        let nat = create_nat()?;
+        let node1 = create_node(&nat, Some("50000"))?;
+        let node2 = create_node(&nat, Some("50000"))?;
+
+        let ep1 = format!("http://127.0.0.1:{}", node1.pub_port.unwrap());
+        let ep2 = format!("http://127.0.0.1:{}", node2.pub_port.unwrap());
+
+        let mut cli1 = Client::new(&ep1).await?;
+        let mut cli2 = Client::new(&ep2).await?;
+
+        let node1_peers = cli1.list_peers().await?.result;
+        let node2_peers = cli2.list_peers().await?.result;
+        assert_eq!(node1_peers.len(), 0);
+        assert_eq!(node2_peers.len(), 0);
+
+        let trans_and_ice1 = cli1.create_offer().await?.result;
+        let trans_and_ice2 = cli2.connect_peer_via_ice(&trans_and_ice1.ice).await?.result;
+        let peer = cli1
+            .accept_answer(&trans_and_ice1.transport_id, &trans_and_ice2.ice)
+            .await?
+            .result;
+
+        assert_eq!(peer.transport_id, trans_and_ice1.transport_id);
+
+        let node1_peers = cli1.list_peers().await?.result;
+        assert_eq!(node1_peers.len(), 1);
+        assert_eq!(node1_peers[0].transport_id, trans_and_ice1.transport_id);
+        assert_eq!(node1_peers[0].address, node2.address()?);
+
+        let node2_peers = cli2.list_peers().await?.result;
+        assert_eq!(node2_peers.len(), 1);
+        assert_eq!(node2_peers[0].transport_id, trans_and_ice2.transport_id);
         assert_eq!(node2_peers[0].address, node1.address()?);
 
         Ok(())
